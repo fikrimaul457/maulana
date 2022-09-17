ad570918f996679d8329dcb16b3da3ac06b0d57a
fikrimaul457 <86140611+fikrimaul457@users.noreply.github.com>
2022-09-17 16:49:19 +0700

    Add files via upload

---
+++ harga total.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define harga 4500.02//..menentukan harga dengan define
+
+int main() { 
+  float jumlah,total;
+  cout << "Masukan Jumlah Barang = ";
+  cin >> jumlah; total = 13500.1;
+  cout << "\n Yang Harus Di Bayar = \n" << total;
+
+  return 0;
+}
